fa975e493869d39b86ebc6ec1eaba4025e56595b
Orosz <eorosz@contactor.usgs.gov>
2019-11-01 13:29:45 -0500

    WQP-1447 (WMA Jenkins jobs not failing with liquibase errors)   Updated the z* script called by jenkins to have an '|| exit 1' in the execute liquidbase command, so that the script will fail out on errors calling the liquidbase command.

--- liquibase/scripts/dbData/z3_load_demo_data.sh
+++ liquibase/scripts/dbData/z3_load_demo_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gunzip ${LIQUIBASE_HOME}/wqp_core_all.tar.gz
+gunzip ${LIQUIBASE_HOME}/wqp_core_all.tar.gz || exit 1
 
-pg_restore --dbname=postgresql://postgres:${POSTGRES_PASSWORD}@${WQP_DATABASE_ADDRESS}:5432/${WQP_DATABASE_NAME} -v -c --if-exists -F t ${LIQUIBASE_HOME}/wqp_core_all.tar
+pg_restore --dbname=postgresql://postgres:${POSTGRES_PASSWORD}@${WQP_DATABASE_ADDRESS}:5432/${WQP_DATABASE_NAME} -v -c --if-exists -F t ${LIQUIBASE_HOME}/wqp_core_all.tar || exit 1
 
-psql -v ON_ERROR_STOP=1 --dbname=postgresql://postgres:${POSTGRES_PASSWORD}@${WQP_DATABASE_ADDRESS}:5432/${WQP_DATABASE_NAME} -c "vacuum full;"
+psql -v ON_ERROR_STOP=1 --dbname=postgresql://postgres:${POSTGRES_PASSWORD}@${WQP_DATABASE_ADDRESS}:5432/${WQP_DATABASE_NAME} -c "vacuum full;" || exit 1
