a794559483a9380dcfc6d57413f54992471d076d
Andrew Yan <ayan@usgs.gov>
2020-08-07 08:06:00 -0500

    drop huc12nometa if it exists

--- sql/huc12_transform.sql
+++ sql/huc12_transform.sql
@@ -1,4 +1,5 @@
 -- Setup the huc12nometa data table from the raw data in hu12_nhdplusv2
+drop table if exists wqp.huc12nometa;
 create table wqp.huc12nometa as select hn.huc_12, hn.wkb_geometry from wqp.hu12_nhdplusv2 hn;
 alter table wqp.huc12nometa rename column wkb_geometry to geometry;
 create index huc12nometa_geom on wqp.huc12nometa using gist (geometry) with (fillfactor='100');
