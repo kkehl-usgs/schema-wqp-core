a3236a672652f5e9819044ddc8cd96051bfee1d4
mbucknell <mbucknell@usgs.gov>
2020-08-11 16:06:53 -0500

    Update huc_12 column to huc12

--- sql/huc12_transform.sql
+++ sql/huc12_transform.sql
@@ -2,5 +2,6 @@
 drop table if exists wqp.huc12nometa;
 create table wqp.huc12nometa as select hn.huc_12, hn.wkb_geometry from wqp.hu12_nhdplusv2 hn;
 alter table wqp.huc12nometa rename column wkb_geometry to geometry;
+alter table wqp.huc12nometa rename column huc_12 to huc12;
 create index huc12nometa_geom on wqp.huc12nometa using gist (geometry) with (fillfactor='100');
 drop table if exists wqp.hu12_nhdplusv2;
