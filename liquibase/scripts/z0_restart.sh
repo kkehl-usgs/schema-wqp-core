d5def92b8b86224796a07fa4c35385ec8690da75
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-11 10:33:13 -0700

    Revert "increase work_mem from 4mb default to 32mb (WQP-1597) (#165)" (#166)
    This reverts commit 500ebb8af7c11e098546a2aff75429ec6c81beca.

--- liquibase/scripts/z0_restart.sh
+++ liquibase/scripts/z0_restart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash 
 
 # Restart postgres to make sure we can connect
-
-psql ${WQP_DATABASE_NAME} -c "alter system set work_mem='32MB'"
 pg_ctl -D "$PGDATA" -m fast -o "$LOCALONLY" -w restart
