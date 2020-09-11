500ebb8af7c11e098546a2aff75429ec6c81beca
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-11 09:19:03 -0700

    increase work_mem from 4mb default to 32mb (WQP-1597) (#165)
    * increase work_mem from 4mb default to 32mb
    
    * remove .idea files

--- liquibase/scripts/z0_restart.sh
+++ liquibase/scripts/z0_restart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
 
 # Restart postgres to make sure we can connect
+
+psql ${WQP_DATABASE_NAME} -c "alter system set work_mem='32MB'"
 pg_ctl -D "$PGDATA" -m fast -o "$LOCALONLY" -w restart
